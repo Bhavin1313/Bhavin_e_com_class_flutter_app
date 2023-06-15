813a8c83954fe078ae518c7df9cdbb2507cb3a47
bhavin bhalala <bhavin1313bhalala@gmail.com>
2023-06-15 13:59:31 -0700

    diteal page

--- lib/Screens/detail_page.dart
+++ lib/Screens/detail_page.dart
@@ -14,7 +14,31 @@ class _Detail_ScreenState extends State<Detail_Screen> {
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Detail Page"),
+        backgroundColor: Colors.white,
+        title: Text(
+          "details Page",
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          Icon(
+            Icons.shopping_cart,
+            color: Colors.black,
+            size: 27,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+        ],
+        centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -24,27 +48,156 @@ class _Detail_ScreenState extends State<Detail_Screen> {
           size: 35,
         ),
       ),
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                ...data['images']
-                    .map((e) => Container(
-                          margin: EdgeInsets.all(10),
-                          height: 350,
-                          width: 350,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ...data['images']
+                      .map(
+                        (e) => Container(
+                          height: 400,
+                          width: 400,
                           decoration: BoxDecoration(
                             image: DecorationImage(
-                                image: NetworkImage(e), fit: BoxFit.cover),
+                              image: NetworkImage(e),
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ))
-                    .toList(),
-              ],
+                        ),
+                      )
+                      .toList(),
+                ],
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              top: 350,
+              child: Container(
+                padding: EdgeInsets.all(12),
+                height: 320,
+                width: 360,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 15,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                  color: Colors.white,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          data['title'],
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "\$ ${data['price']}",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.redAccent,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          data['title'],
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "⭐ ${data['rating']}",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Brand",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "${data['brand']}",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Stock",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "${data['stock']}",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
+                    Text(
+                      "Description",
+                      style:
+                          TextStyle(fontSize: 19, fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      "${data['description']}",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
